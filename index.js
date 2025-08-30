948b57e7d22ae8029b822203b440b0c64ae722b7
Anhelina Priadko <anhelina.pryadko@gmail.com>
2025-08-30 12:40:55 +0300

    remove unnnecessary comments

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const basicURL = "https://bored-api.appbrewery.com/";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(basicURL + "random");
